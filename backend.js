ff11c77899e4a3ecfe3769297563af0458e8591f
John Alexis Guerra Gómez <john.guerra@gmail.com>
2023-10-12 17:26:37 -0700

    Second class work with create and delete

--- backend.js
+++ backend.js
@@ -1,12 +1,23 @@
 import express from "express";
 
-import apiRouter from "./routes/api.js";
+import bodyParser from "body-parser";
+
+import apiRouterPrompts from "./routes/apiPrompts.js";
+import apiRouterUsers from "./routes/apiUsers.js";
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("frontend"));
-app.use("/api", apiRouter);
+app.use("/api/prompts", apiRouterPrompts);
+app.use("/api/users", apiRouterUsers);
 
 app.listen(PORT, () => console.log(`First Listening on port ${PORT}`));
--- frontend/js/frontend.js
+++ frontend/js/frontend.js
@@ -1,13 +1,15 @@
 function FrontEnd() {
   const me = {};
 
+  const formCreatePrompt = document.querySelector("#formCreatePrompt");
+
   me.showMessage = function (message, type = "danger") {
     const messagesDiv = document.querySelector("#messages");
 
     const wrapper = document.createElement("div");
     wrapper.innerHTML = `<div class="alert alert-${type} alert-dismissible" role="alert">
          <div>${message}</div>
-         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>`;
 
     messagesDiv.append(wrapper);
@@ -26,19 +28,81 @@ function FrontEnd() {
     me.renderPrompts(prompts);
   };
 
+  // Returns a callback to delete the prompt that is passed as a parameter
+  function onDeletePrompt(prompt) {
+    return async function () {
+      const res = await fetch("/api/prompts/delete", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ _id: prompt._id }),
+      });
+
+      if (res.ok) {
+        me.showMessage("Prompt deleted", "warning");
+        me.reloadPrompts();
+      } else {
+        me.showMessage("Error deleting prompt", "danger");
+        return;
+      }
+    };
+  }
+
+  // Takes a prompt from the database and returns a
+  // html element of it
   const renderPrompt = function (prompt) {
-    return `<div>
+    const pElement = document.createElement("div");
+
+    pElement.innerHTML = `
       <label>Prompt: ${prompt.prompt} </label>
       <label>Rating: ${prompt.rating} </label>
-    
-    </div>`;
+    `;
+
+    const btnDeletePrompt = document.createElement("button");
+
+    btnDeletePrompt.innerText = "🚫";
+    btnDeletePrompt.classList.add("btn", "btn-outline-danger", "btn-sm");
+    btnDeletePrompt.addEventListener("click", onDeletePrompt(prompt));
+
+    pElement.appendChild(btnDeletePrompt);
+
+    return pElement;
   };
 
   me.renderPrompts = function (prompts) {
     const promptsDiv = document.querySelector("#prompts");
 
-    promptsDiv.innerHTML = prompts.map(renderPrompt).join("\n");
+    promptsDiv.innerHTML = "";
+
+    for (let p of prompts) promptsDiv.appendChild(renderPrompt(p));
   };
+
+  async function onCreatePrompt(evt) {
+    // Avoids re rendering
+    evt.preventDefault();
+
+    const formData = new FormData(formCreatePrompt);
+    console.log("onCreatePrompt", evt, "formData", formData);
+
+    const res = await fetch("/api/prompts/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(Object.fromEntries(formData.entries())),
+    });
+
+    if (res.ok) {
+      me.showMessage("Prompt created", "success");
+      formCreatePrompt.reset();
+      me.reloadPrompts();
+    } else {
+      me.showMessage("Error creating prompt", "danger");
+    }
+  }
+
+  formCreatePrompt.addEventListener("submit", onCreatePrompt);
 
   return me;
 }
